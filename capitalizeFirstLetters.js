9c843ca27016ee9ec0ed47410e3c6baa0aff9b54
Robert <janssensrobert@hotmail.com>
2020-03-07 11:14:45 +0100

    function written

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,3 +1,16 @@
+function capitalizeFirstLetters(argument) {
+	// test5 empty string
+	if (argument.length===0) {
+		return argument
+	// test4 simple string
+	} else if (argument.indexOf(' ')<0){
+		return argument.replace(argument[0], argument[0].toUpperCase())
+	// test3 chain of string
+	} else {
+		return argument.split(' ').map(item=>item.replace(item[0], item[0].toUpperCase())).join(' ')
+	}
+}
+
 
 const assert = require('assert');
 // test1 if capitalizeFirstLetters() is a function
